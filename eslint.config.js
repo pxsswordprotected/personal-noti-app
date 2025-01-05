0ec8901b72ea8e09eb6d3a7e37fc43b8ea9ff8f3
pxsswordprotected <jamesalford661@gmail.com>
2025-01-05 11:15:43 -0600

    lint debug

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,22 @@
 // eslint.config.js
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-
-export default [
-    js.configs.recommended,
-    prettier,
-    {
-        env: {
-            browser: true,
-            es2021: true,
-            node: true,
-            jest: true
-        },
-        parserOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module'
-        }
+export default {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true
+    },
+    extends: [
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always']
     }
-];
+};
