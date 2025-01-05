615eca54ed99a10747aacd2a253fcc609d14f1ed
pxsswordprotected <jamesalford661@gmail.com>
2025-01-05 11:05:01 -0600

    lint debugging

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
-module.exports = [
+import eslintRecommended from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
     {
         files: ["**/*.js"],
         languageOptions: {
             ecmaVersion: "latest",
             sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.jest,
+            },
         },
-        env: {
-            browser: true,
-            es2021: true,
-            node: true,
-            jest: true,
+        rules: {
+            ...eslintRecommended.rules, // Include ESLint's recommended rules
+            ...prettierConfig.rules,   // Include Prettier rules
         },
-        extends: [
-            "eslint:recommended",
-            "prettier"
-        ],
     },
 ];
